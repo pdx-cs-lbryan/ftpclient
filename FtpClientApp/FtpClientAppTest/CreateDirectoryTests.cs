0a4b459ed43a6b855030dcfbf20dc74d6a7577a5
Bryan <lbryan@pdx.edu>
2019-07-26 00:35:00 -0700

    create failing test

--- FtpClientApp/FtpClientAppTest/CreateDirectoryTests.cs
+++ FtpClientApp/FtpClientAppTest/CreateDirectoryTests.cs
@@ -4,7 +4,40 @@ using System.Text;
 
 namespace FtpClientAppTest
 {
-    class CreateDirectoryTests
+    using FakeItEasy;
+    using FtpClientApp;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Net;
+
+    [TestClass]
+    public class CreateDirectoryTests
     {
+        public CreateDirectoryTests()
+        {
+
+        }
+
+        [TestMethod]
+        public void directoryAlreadyExistsReturnsMessageSayingItExists()
+        {
+            var info = A.Fake<ServerConnectionInformation>();
+            var crd = A.Fake<CreateRemoteDirectory>();
+            var server = A.Fake <FtpWebRequest> ();
+
+            info.UserName = "un";
+            info.PassWord = "pw";
+            info.ServerName = "ftp://localhost";
+
+            WebException ex = new WebException(
+                "testmsg", (WebExceptionStatus) FtpStatusCode.ActionNotTakenFileUnavailable);
+          
+
+            A.CallTo(() => crd.getDirectoryName()).Returns("testdir");
+            A.CallTo(() => server.GetResponse()).Throws(ex);
+            String resp = crd.create(crd.getDirectoryName());
+            Assert.IsTrue(resp.Equals("The server sent an error code of 550. The directory may already exist or the file was unavailable due to a lack of access."));
+
+        }
+        
     }
 }
