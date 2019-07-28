d0cf829fec8ef50e5d7d8d669ad7770c66e6eea8
mbillett <52435703+mbillett@users.noreply.github.com>
2019-07-28 15:44:30 -0700

    Add files via upload

---
+++ FtpClientApp/FtpClientApp/FTPTestWrapper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace FtpClientApp
+{
+    //Make a mock of this with your tests
+    abstract public class FTPTestWrapperAbstract
+    {
+        abstract public FtpWebResponse getResp();
+        
+    }
+
+
+    //Use This wrapper to make your request
+    //You'll need to set the request first with setRequest, then invoke getResp
+    public class FtpTestWrapper : FTPTestWrapperAbstract
+    {
+        FtpWebRequest req = null;
+
+        public void setRequest(FtpWebRequest request)
+        {
+            this.req = request;
+        }
+
+        override
+        public FtpWebResponse getResp()
+        {
+            try
+            {
+
+                return (FtpWebResponse)this.req.GetResponse();
+            }
+            catch (Exception e)
+            {
+                throw e;
+
+            } 
+        }
+    }
+}
