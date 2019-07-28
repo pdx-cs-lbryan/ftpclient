6db58c3f4d8107f1c990bcf546f036d4d6bb7189
mbillett <52435703+mbillett@users.noreply.github.com>
2019-07-27 20:11:18 -0700

    start of file upload class

--- FtpClientApp/FtpClientApp/FileUpload.cs
+++ FtpClientApp/FtpClientApp/FileUpload.cs
@@ -1,18 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Linq;
+using System.IO;
 using System.Net;
+using System.Text;
 
 namespace FtpClientApp
 {
     class FileUpload
     {
         public FileUpload(ServerConnectionInformation myConnection)
-        {
+        {   //for test
+            myConnection.ServerName = "tekprd05:/u/prodctl/kintana/upload";
+            myConnection.UserName = "kintana";
+            myConnection.PassWord = "";
             Console.WriteLine("\n ServerName:  " + myConnection.ServerName);
             Console.WriteLine("\n UserName:  " + myConnection.UserName);
             Console.WriteLine("\n Password:  " + myConnection.PassWord);
+
+            string FileName = "c:/download/daffodils.jpg";
+
+            //FtpWebRequest myServerConnectionRequest = (FtpWebRequest)WebRequest.Create(myConnection.ServerName);
+            FtpWebRequest myServerConnectionRequest = (FtpWebRequest)WebRequest.Create(new Uri(myConnection.ServerName));
+            myServerConnectionRequest.Credentials = new NetworkCredential(myConnection.UserName, myConnection.PassWord);
+            myServerConnectionRequest.Proxy = null;
+            myServerConnectionRequest.UsePassive = true;
+            myServerConnectionRequest.UseBinary = true;
+            myServerConnectionRequest.KeepAlive = true;
+
+            myServerConnectionRequest.Method = WebRequestMethods.Ftp.UploadFile;
+
+            byte[] upLoadFileContents;
+            using (StreamReader sourceStream = new StreamReader(FileName))
+            {
+                upLoadFileContents = Encoding.UTF8.GetBytes(sourceStream.ReadToEnd());
+            }
+
+            myServerConnectionRequest.ContentLength = upLoadFileContents.Length;
+
+            using (Stream UploadStream = myServerConnectionRequest.GetRequestStream())
+            {
+                UploadStream.Write(upLoadFileContents, 0, upLoadFileContents.Length);
+            }
+            using (FtpWebResponse resultsResponse = (FtpWebResponse)myServerConnectionRequest.GetResponse())
+            {
+                Console.WriteLine($"File upload to destination status: {resultsResponse.StatusDescription}");
+            }
 
         } // end FileUPload()
 
