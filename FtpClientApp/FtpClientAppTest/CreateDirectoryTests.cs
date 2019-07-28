61e22a6ad39d4e5245da4910b172e646c7d0078c
mbillett <52435703+mbillett@users.noreply.github.com>
2019-07-28 15:43:50 -0700

    Add files via upload

---
+++ FtpClientApp/FtpClientAppTest/CreateDirectoryTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FtpClientAppTest
+{
+    using FakeItEasy;
+    using FtpClientApp;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Net;
+
+    [TestClass]
+    public class CreateDirectoryTests
+    {
+        public CreateDirectoryTests()
+        {
+
+        }
+
+        [TestMethod]
+        public void directoryAlreadyExistsReturnsMessageSayingItExists()
+        {
+            var info = A.Fake<ServerConnectionInformation>();
+            var server = A.Fake <FTPTestWrapperAbstract> ();
+            
+            info.UserName = "un";
+            info.PassWord = "pw";
+            info.ServerName = "ftp://localhost";
+            WebException ex = new WebException(
+                "The remote server returned an error: (550) File unavailable (e.g., file not found, no access).",
+                WebExceptionStatus.ProtocolError);
+            
+
+            Console.WriteLine(ex.Message);
+            A.CallTo(() => server.getResp()).Throws(ex);
+            CreateRemoteDirectory crd = new CreateRemoteDirectory(info);
+            String resp = crd.create(server);
+            Console.WriteLine(resp);
+            Assert.IsTrue(resp.Equals("The server sent an error code of 550. The directory may already exist or the file was unavailable due to a lack of access."));
+
+        }
+
+        [TestMethod]
+        public void directoryNotExistsNoError()
+        {
+            var info = A.Fake<ServerConnectionInformation>();
+            var server = A.Fake<FTPTestWrapperAbstract>();
+
+            info.UserName = "un";
+            info.PassWord = "pw";
+            info.ServerName = "ftp://localhost";
+            FtpWebResponse a = null;
+
+
+            A.CallTo(() => server.getResp()).Returns(a);
+            CreateRemoteDirectory crd = new CreateRemoteDirectory(info);
+            String resp = crd.create(server);
+            Assert.IsTrue(resp.Equals("success"));
+        }
+
+        [TestMethod]
+        public void invalidCharReturnsInvalidDir()
+        {
+            var info = A.Fake<ServerConnectionInformation>();
+            var server = A.Fake<FTPTestWrapperAbstract>();
+
+            info.UserName = "un";
+            info.PassWord = "pw";
+            info.ServerName = "ftp://localhost";
+            FtpWebResponse a = null;
+
+            CreateRemoteDirectory crd = new CreateRemoteDirectory(info);
+            bool test = crd.validDir("hello,this|is<not>valid");
+            Assert.IsFalse(test);
+        }
+        
+    }
+}
---
+++ FtpClientApp/FtpClientAppTest/DeleteFromRemoteTests.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FtpClientAppTest
+{
+    using FakeItEasy;
+    using FtpClientApp;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Net;
+
+    [TestClass]
+    public class DeleteFromRemoteTests
+    {
+        public DeleteFromRemoteTests()
+        {
+
+        }
+
+        [TestMethod]
+        public void FileExistsReturnSuccess()
+        {
+            var info = A.Fake<ServerConnectionInformation>();
+            var server = A.Fake<FTPTestWrapperAbstract>();
+
+            info.UserName = "un";
+            info.PassWord = "pw";
+            info.ServerName = "ftp://localhost";
+            WebException ex = new WebException(
+                "The remote server returned success (file found)",
+                WebExceptionStatus.ProtocolError);
+
+
+            Console.WriteLine(ex.Message);
+            A.CallTo(() => server.getResp()).Throws(ex);
+            DeleteFromRemote crd = new DeleteFromRemote(info);
+            String resp = crd.create(server);
+            Console.WriteLine(resp);
+            Assert.IsTrue(resp.Equals("The remote server returned success (file found)"));
+
+        }
+
+        [TestMethod]
+        public void FileNotExistsError()
+        {
+            var info = A.Fake<ServerConnectionInformation>();
+            var server = A.Fake<FTPTestWrapperAbstract>();
+
+            info.UserName = "un";
+            info.PassWord = "pw";
+            info.ServerName = "ftp://localhost";
+            FtpWebResponse a = null;
+
+
+            A.CallTo(() => server.getResp()).Returns(a);
+            DeleteFromRemote crd = new DeleteFromRemote(info);
+            String resp = crd.create(server);
+            Assert.IsTrue(resp.Equals("success"));
+        }
+
+    }
+}
